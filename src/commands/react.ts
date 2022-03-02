d6d3ae9648c17e00926e64b1033503e52b5772dc
Muhammad Kashif Mughal <muhammadkashifmughal.b18101076@gmail.com>
2022-03-02 17:51:48 +0500

    loader added

--- src/commands/react.ts
+++ src/commands/react.ts
@@ -1,5 +1,23 @@
 import { Command, Flags } from "@oclif/core";
-const { execSync } = require("child_process");
+const loading =  require('loading-cli');
+const { exec } = require("child_process");
+
+var execCommand = async (command: string) => {
+  return new Promise((resolve, reject) => {
+    exec(command, (error: any) => {
+      if (error) reject(error);
+      resolve("Success");
+    });
+  });
+};
+
+var stopLoader: Function;
+var startLoader: Function = (text: string, finalText: string = 'done') => {
+  const load = loading(text).start();
+  stopLoader = () => {
+    load.succeed(finalText);
+  };
+}
 
 export default class React extends Command {
   static description = "Create react base project's boilerplate";
@@ -31,18 +49,19 @@ export default class React extends Command {
 
   public async run(): Promise<void> {
     const { args, flags } = await this.parse(React);
-
     if (flags.typeScript) {
       if (this.repo.ts)
-        execSync(`git clone --depth 1 ${this.repo.ts} ${args["Project-Name"]}`);
+        exec(`git clone --depth 1 ${this.repo.ts} ${args["Project-Name"]}`);
       else this.error("TypeScript version isn't available yet.", { exit: 1 });
       process.exit(1);
-    } else {      
-      this.log("Downloading files ....");
-      execSync(`git clone --depth 1 ${this.repo.url} ${args["Project-Name"]}`);
-      this.log("fetching dependencies ....");
+    } else {
+      startLoader("Downloading files ....", "All files downloaded successfully.");
+      await execCommand(`git clone --depth 1 ${this.repo.url} ${args["Project-Name"]}`);
+      stopLoader();
+      startLoader("Installing dependencies, this will take a while ....", "fetched all dependencies successfully.");
       process.chdir(args["Project-Name"]);
-      execSync("npm i");
+      await execCommand(`npm i`)
+      stopLoader();
       this.log("\n   Successfully done.\n");
     }
   }
